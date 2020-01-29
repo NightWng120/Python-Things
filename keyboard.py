397f816829d5c9af089b2567c1a12126119372d8
NightWng120 <treverholmes9@gmail.com>
2020-01-29 14:04:09 -0500

    We sendin ip's bois

---
+++ keyboard.py
@@ -0,0 +1,67 @@
+from pynput.keyboard import Key, Controller
+from bs4 import BeautifulSoup
+from itertools import tee, islice, chain
+import requests
+import time
+
+def previous_and_next(some_iterable):
+    prevs, items, nexts = tee(some_iterable, 3)
+    prevs = chain([None], prevs)
+    nexts = chain(islice(nexts, 1, None), [None])
+    return zip(prevs, items, nexts)
+source = requests.get("https://www.google.com/search?q=what%27s+my+ip&rlz=1C1CHBF_enUS806US806&oq=what%27s+my+ip&aqs=chrome..69i57j0l7.5523j1j4&sourceid=chrome&ie=UTF-8").text;
+
+soup = BeautifulSoup(source,'lxml');
+thing = soup.findAll("div", {'class': ['BNeawe', 'iBp4i', 'AP7Wnd']});
+print(thing[0].text);
+
+
+keyboard = Controller()
+
+time.sleep(2)
+with keyboard.pressed(Key.cmd):
+	keyboard.press('r')
+	keyboard.release('r')
+time.sleep(2)
+keyboard.press(Key.enter);
+keyboard.release(Key.enter)
+time.sleep(5)
+for char in "gmail.com":
+	keyboard.press(char)
+	keyboard.release(char)
+	time.sleep(.1);
+time.sleep(1)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+time.sleep(10)
+i = 11
+while i > 0:
+	keyboard.press(Key.tab)
+	keyboard.release(Key.tab)
+	time.sleep(.1)
+	i = i - 1
+time.sleep(3)
+keyboard.press(Key.enter)
+time.sleep(3)
+for char in "trevholm@iu.edu":
+	keyboard.press(char)
+	keyboard.release(char)
+	time.sleep(.1)
+time.sleep(3)
+i = 3
+while i > 0:
+	keyboard.press(Key.tab)
+	keyboard.release(Key.tab)
+	time.sleep(.2)
+	i = i - 1
+time.sleep(3)
+for char in thing[0].text:
+	keyboard.press(char)
+	keyboard.release(char)
+	time.sleep(.1)
+time.sleep(1)
+keyboard.press(Key.tab)
+keyboard.release(Key.tab)
+time.sleep(1)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
--- scraping.py
+++ scraping.py
@@ -10,7 +10,6 @@ def previous_and_next(some_iterable):
 source = requests.get("https://www.google.com/search?q=what%27s+my+ip&rlz=1C1CHBF_enUS806US806&oq=what%27s+my+ip&aqs=chrome..69i57j0l7.5523j1j4&sourceid=chrome&ie=UTF-8").text;
 
 soup = BeautifulSoup(source,'lxml');
-for previous, div, nxt in previous_and_next(soup.find_all("div")):
-	if div.text == "Your public IP address":
-		print(previous.text);
+thing = soup.findAll("div", {'class': ['BNeawe', 'iBp4i', 'AP7Wnd']});
+print(thing[0].text);
 		
